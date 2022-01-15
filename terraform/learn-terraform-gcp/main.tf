27896fc32d11edceed0690b2b62dd9a7eb2e702c
root <anildursun@gmail.com>
2022-01-15 22:42:58 +0100

    new files

---
+++ terraform/learn-terraform-gcp/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/mnt/gcp/causal-fort-336300-74e2b860c8fa.json")
+
+  project = "causal-fort-336300"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
